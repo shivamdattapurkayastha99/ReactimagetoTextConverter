4df05c551672ef195b24a639abdbe5df11321790
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-09-25 16:55:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
+import ImageWrapper from './Components/ImageWrapper';
+import TextWrapper from './Components/TextWrapper';
+import axios from "axios";
+const API_KEY="f3c4921518f6a42a65233aff576d5365";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [text, setText] = useState(null);
+
+  const convertImageToText = async () => {
+    setLoading(true);
+    const result = await Tesseract.recognize(imageUrl, "eng");
+    setText(result.data.text);
+    setLoading(false);
+  };
+
+  const uploadFile = async e => {
+    try {
+      setLoading(true);
+      const formData = new FormData();
+      formData.append("image", e.target.files[0]);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+
+      const res = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${API_KEY}`,
+        formData,
+        config
+      );
+      setImageUrl(res.data.data.url);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (imageUrl != null) {
+      convertImageToText();
+    }
+  }, [imageUrl]);
+  console.log(`${process.env.REACT_APP_API_KEY}`);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shivam Image To Text Converter</h1>
+      {/* <img src="imagetotextapp/static/myphoto.png" className="logo" width="200px" height="200px" alt="Shivam Image To Text Converter" /> */}
+      <div className="container">
+      {loading && <div className="loader"></div>}
+        {text == null ? (
+          <ImageWrapper loading={loading} uploadFile={uploadFile} />
+        ) : (
+          <TextWrapper text={text} />
+        )}
+      </div>
     </div>
   );
 }
